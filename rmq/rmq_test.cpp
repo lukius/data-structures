1b12df8793b75897139e284483bb143f2c12aff1
Lucio Santi <lukius@gmail.com>
2016-02-20 18:56:43 -0300

    Tabs.

--- rmq/rmq_test.cpp
+++ rmq/rmq_test.cpp
@@ -13,38 +13,38 @@ TEST(RMQTest, naive_rmq_test)
 {
 	NaiveRMQ<int> naive_rmq(A);
 
-    EXPECT_EQ(naive_rmq(0,9), -10);
-    EXPECT_EQ(naive_rmq(2,2), 22);
-    EXPECT_EQ(naive_rmq(1,4), 14);
-    EXPECT_EQ(naive_rmq(7,9), 23);
+	EXPECT_EQ(naive_rmq(0,9), -10);
+	EXPECT_EQ(naive_rmq(2,2), 22);
+	EXPECT_EQ(naive_rmq(1,4), 14);
+	EXPECT_EQ(naive_rmq(7,9), 23);
 }
 
 TEST(RMQTest, fully_precomputed_rmq_test)
 {
 	FullyPrecomputedRMQ<int> full_rmq(A);
 
-    EXPECT_EQ(full_rmq(0,9), -10);
-    EXPECT_EQ(full_rmq(2,2), 22);
-    EXPECT_EQ(full_rmq(1,4), 14);
-    EXPECT_EQ(full_rmq(7,9), 23);
+	EXPECT_EQ(full_rmq(0,9), -10);
+	EXPECT_EQ(full_rmq(2,2), 22);
+	EXPECT_EQ(full_rmq(1,4), 14);
+	EXPECT_EQ(full_rmq(7,9), 23);
 }
 
 TEST(RMQTest, block_rmq_test)
 {
 	BlockRMQ<int> block_rmq(A);
 
-    EXPECT_EQ(block_rmq(0,9), -10);
-    EXPECT_EQ(block_rmq(2,2), 22);
-    EXPECT_EQ(block_rmq(1,4), 14);
-    EXPECT_EQ(block_rmq(7,9), 23);
+	EXPECT_EQ(block_rmq(0,9), -10);
+	EXPECT_EQ(block_rmq(2,2), 22);
+	EXPECT_EQ(block_rmq(1,4), 14);
+	EXPECT_EQ(block_rmq(7,9), 23);
 }
 
 TEST(RMQTest, sparse_rmq_test)
 {
 	SparseTableRMQ<int> sparse_rmq(A);
 
-    EXPECT_EQ(sparse_rmq(0,9), -10);
-    EXPECT_EQ(sparse_rmq(2,2), 22);
-    EXPECT_EQ(sparse_rmq(1,4), 14);
-    EXPECT_EQ(sparse_rmq(7,9), 23);
+	EXPECT_EQ(sparse_rmq(0,9), -10);
+	EXPECT_EQ(sparse_rmq(2,2), 22);
+	EXPECT_EQ(sparse_rmq(1,4), 14);
+	EXPECT_EQ(sparse_rmq(7,9), 23);
 }
